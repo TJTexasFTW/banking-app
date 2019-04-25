29319e0c342f20345f3c55d9c2583602bb959786
Theresa Stocking <nottodayjose@MacBook-Pro.local>
2019-04-25 10:20:59 -0500

    Routes set up

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {HashRouter} from 'react-router-dom';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                Login
+            </div>
+        )
+    }
+}
+
+
+// function Login() {
+//   return (
+//     <HashRouter>
+//       <div className="Login">
+//         <input />
+//         <input type='password'/>
+//       </div>
+//     </HashRouter>
+//   );
+// }
+
+export default Login;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Login from './components/Login';
+
+export default (
+
+    <Switch>
+
+        <Route path='/' component={Login} />
+
+    </Switch>
+
+)
